cedb9301320e04bd2c00951375e4862688140e29
marina-fares <marina.fares11@gmail.com>
2020-05-18 18:34:07 +0200

    add order and menu classes

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Menu(models.Model):
+    type_i = models.CharField(max_length=100)
+    item = models.CharField(max_length=100)
+    price_small = models.IntegerField(null=True)
+    price_large = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.item
+
+class Order(models.Model):
+    customer = models.ForeignKey( User, on_delete=models.CASCADE)
+    item_id = models.ManyToManyField(Menu)
+    datetime = models.DateTimeField(default=timezone.now) 
+
+    
+    
