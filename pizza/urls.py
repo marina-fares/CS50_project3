13c4f69cc1dc17f1feb046c860da9511161f9020
marina-fares <marina.fares11@gmail.com>
2020-05-18 22:39:51 +0200

    submit registration form

--- pizza/urls.py
+++ pizza/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from users import views as user_view
+from users import views
+
 
 urlpatterns = [
     path("", include("orders.urls")),
+    path("", include("users.urls")),
     path("admin/", admin.site.urls),
+
 ]
---
+++ users/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .form import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form}) 
+
+def home(request):
+    return render(request, 'users/home.html')
+
+'''
+message.debug
+message.info
+message.success
+message.warning
+message.error
+'''
