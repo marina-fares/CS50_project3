73dd60798a95e21255defeaa28cbceb8a27af9c8
marina-fares <marina.fares11@gmail.com>
2020-05-20 16:44:29 +0200

    commit layout design

--- pizza/urls.py
+++ pizza/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-
+from orders import views
 
 urlpatterns = [
     path("", include("orders.urls")),
     path("", include("users.urls")),
     path("admin/", admin.site.urls),
- 
 ]
